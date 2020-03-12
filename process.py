e563ab3737c9aeeef40137a2311c1674da191215
Nono Martínez Alonso <mundowarezweb@gmail.com>
2020-03-12 09:23:15 +0100

    Add 2.x note.

--- process.py
+++ process.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import tensorflow as tf
+import tensorflow as tf # 2.x
 import base64
 import json
 import numpy as np
